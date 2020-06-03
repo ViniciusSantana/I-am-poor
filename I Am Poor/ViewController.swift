92da01b20cb80f11c94a9801fe44453825a948ee
Vinicius <vinicius.santana@trustly.com>
2020-06-03 19:06:10 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Vinicius Santana on 03/06/20.
+//  Copyright © 2020 Vinicius Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
